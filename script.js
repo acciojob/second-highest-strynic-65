1627b7bd8399a851839eecc3663b67df71cf3c53
Aditya Pratap Singh <159929948+strynic-65@users.noreply.github.com>
2025-06-24 11:10:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,9 +16,3 @@ function secondHighest(arr) {
   return second === -Infinity ? -Infinity : second;
 }
 
-// Test cases
-console.log(secondHighest([5, 1, 2, 3, 4]));     // Output: 4
-console.log(secondHighest([-1, -2, -3, -4]));    // Output: -2
-console.log(secondHighest([]));                 // Output: -Infinity
-console.log(secondHighest([1]));                // Output: -Infinity
-console.log(secondHighest([1, 1, 1]));           // Output: -Infinity
